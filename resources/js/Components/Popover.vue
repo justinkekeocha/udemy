9a30f2b29d19351f7227f40e5d221f32af879de7
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-22 22:43:40 +0100

    Changed z-index to 50. removed some classes from popover

--- resources/js/Components/Popover.vue
+++ resources/js/Components/Popover.vue
@@ -4,7 +4,7 @@ defineProps(['target'])
 
 <template>
    <div data-popover :id="target"
-      class="absolute z-10 invisible opacity-0 inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 shadow-sm"
+      class="absolute z-50 invisible opacity-0 inline-block transition-opacity duration-300 bg-white border border-gray-200 shadow-sm"
       role="tooltip">
       <div class="px-3 py-2">
          <slot>
