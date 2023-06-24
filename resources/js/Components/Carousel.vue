9e6f0eb21050d2ea7e0d37e55b8f8c00e764566a
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-24 23:48:58 +0100

    attributes should fall through to the first child element, for better control of height

--- resources/js/Components/Carousel.vue
+++ resources/js/Components/Carousel.vue
@@ -1,11 +1,14 @@
 <script setup>
 import CarouselControls from "../Components/Buttons/CarouselControls.vue";
+defineOptions({
+    inheritAttrs: false
+})
 </script>
 <template>
     <div class="relative w-full" data-carousel="static">
         <!-- Carousel wrapper -->
         <!--h-56 md:h-96-->
-        <div class="relative h-[26rem] overflow-hidden ">
+        <div class="relative h-[26rem] overflow-hidden " v-bind="$attrs">
             <!--Items-->
             <slot></slot>
         </div>
