4af1ea8fe2df53547f0ad00606a06b0b716b732a
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-25 23:24:46 +0100

    Initialize flowbite from layout

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -1,7 +1,14 @@
 <script setup>
 import Navbar from './Navbar.vue'
 import Footer from './Footer.vue';
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
 defineProps(['title', 'description'])
+//https://flowbite.com/docs/getting-started/vue/#data-attributes
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <template>
@@ -10,7 +17,10 @@ defineProps(['title', 'description'])
         <meta name="description" :content="description" />
         <link href="/favicon.png" rel="icon">
     </Head>
+
     <Navbar />
+
+
     <slot />
     <Footer></Footer>
 </template>
