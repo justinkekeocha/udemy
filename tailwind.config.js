dc74db709178cc55919f89e2c9f53ca4f9843a5c
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-25 23:28:14 +0100

    added another font and shotened code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
+//https://flowbite.com/docs/getting-started/vue/
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
+    "./resources/**/*.{vue,js,ts,jsx,tsx,blade.php}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       fontFamily: {
         /**Udemy sans is defined in app.css */
-        'UdemySans': ['UdemySans'],
-        'SuisseWorks' : ['SuisseWorks'],
-      }
+        UdemySans: ["UdemySans"],
+        UdemySansBold: ["UdemySansBold"],
+        SuisseWorks: ["SuisseWorks"],
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
