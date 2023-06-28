b257b0ee0e144f25b9707a4dbea7833e09d505f5
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-28 23:32:48 +0100

    created new component for courses slide

---
+++ resources/js/Components/Slides/CoursesSlide.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Slide from './Slide.vue';
+import StarRating from '../StarRating.vue'
+defineProps({ courses: Object })
+</script>
+<template>
+    <Slide>
+        <template v-for="(row, index) in courses" :key="row.id">
+            <div class="flex flex-col">
+                <Link class="my-5" :href="row.link">
+                <img class="w-full h-32 object-cover border border-stone-400 hover:transition-opacity hover:opacity-80"
+                    :src="row.image" alt="" />
+                <div class="p-5">
+                    <h3 class="text-base line-clamp-2 text-ellipsis tracking-tight mb-0">
+                        {{ row.title }}
+                    </h3>
+                    <p class="text-xs line-clamp-1 text-ellipsis text-gray-400">{{
+                        row.instructor.name }}</p>
+                    <div class="flex">
+                        <span class="text-sm font-bold text-yellow-600">4.6</span>
+                        <StarRating />
+                        <span class="text-gray-400 text-xs ms-1">(499,666) </span>
+                    </div>
+                    <p class="mb-3 font-bold">₦ {{ row.price }} <span
+                            class="line-through text-gray-400 text-sm font-normal ms-1">{{
+                                row.inflated_price }}</span>
+                    </p>
+                </div>
+                </Link>
+            </div>
+        </template>
+    </Slide>
+</template>
