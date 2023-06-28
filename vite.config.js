7bd5a8df004b79c657d7f5a5625ba3486af28466
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-28 23:37:44 +0100

    formating

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import vue from "@vitejs/plugin-vue"; //add this line
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        vue(), // write this
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    vue(), // write this
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
+  ],
 });
 
