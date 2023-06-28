c2e3a123fe9a5d317cddcba82ad8729123c3f75c
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-28 23:31:55 +0100

    removed padding and adjusted the controls

--- resources/js/Components/Buttons/CarouselControls.vue
+++ resources/js/Components/Buttons/CarouselControls.vue
@@ -14,12 +14,12 @@ const props = defineProps(['previousControlCustom', 'nextControlCustom'])
 </script>
 <template>
     <button type="button"
-        class="absolute top-0 left-0 z-20 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        class="absolute top-0 left-2 z-20 flex items-center justify-center h-full cursor-pointer group focus:outline-none"
         :class="previousControlCustom" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-black
+        <span class="inline-flex items-center justify-center w-12 h-12 rounded-full bg-black
         dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white
         dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white font-black sm:w-6 sm:h-6 dark:text-gray-800" fill="none"
+            <svg aria-hidden="true" class="w-6 h-6 text-white font-black  dark:text-gray-800" fill="none"
                 stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7">
                 </path>
@@ -28,13 +28,13 @@ const props = defineProps(['previousControlCustom', 'nextControlCustom'])
         </span>
     </button>
     <button type="button"
-        class="absolute top-0 right-0 z-20 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        class="absolute top-0 right-2 z-20 flex items-center justify-center h-full cursor-pointer group focus:outline-none"
         :class="nextControlCustom" data-carousel-next>
-        <span class=" inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-black
+        <span class=" inline-flex items-center justify-center w-12 h-12 rounded-full bg-black
     dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white
     dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none"
-                stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg aria-hidden="true" class="w-6 h-6 text-white  dark:text-gray-800" fill="none" stroke="currentColor"
+                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
             </svg>
             <span class="sr-only">Next</span>
