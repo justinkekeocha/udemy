58d6ebaae03a715b121cb31f117d15809b8099a2
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-28 23:33:48 +0100

    turned 'data-carousel' attribute to a prop and defined a default value.

--- resources/js/Components/Carousel.vue
+++ resources/js/Components/Carousel.vue
@@ -3,9 +3,16 @@ import CarouselControls from "../Components/Buttons/CarouselControls.vue";
 defineOptions({
     inheritAttrs: false
 })
+defineProps({
+    dataCarousel: {
+        type: String,
+        default: 'static'
+    }
+
+})
 </script>
 <template>
-    <div class="relative w-full" data-carousel="static">
+    <div class="relative w-full" :data-carousel="dataCarousel">
         <!-- Carousel wrapper -->
         <!--h-56 md:h-96-->
         <div class="relative h-[26rem] overflow-hidden " v-bind="$attrs">
