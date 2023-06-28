88118e6caf9ac2f9db24fa7b558be7183e913a77
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-28 23:31:24 +0100

    created has many through relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Category extends Model
 {
@@ -21,6 +22,11 @@ class Category extends Model
     public function subCategories()
     {
         return $this->hasMany(SubCategory::class);
+    }
+
+    public function courses(): HasManyThrough
+    {
+        return $this->hasManyThrough(Course::class, SubCategory::class);
     }
 
     //Attributes
