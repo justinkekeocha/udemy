bd68e6a8de0d883ac0ff5598dbca74d6deb45690
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-29 22:13:16 +0100

    overhauled the whole code so height wont have to be specified

--- resources/js/Components/Carousel.vue
+++ resources/js/Components/Carousel.vue
@@ -1,27 +1,40 @@
 <script setup>
-import CarouselControls from "../Components/Buttons/CarouselControls.vue";
+import { onMounted } from "vue";
+import CarouselControls from './Buttons/CarouselControls.vue'
+import { useSlide } from "../Composables/Slide";
+
+onMounted(() => {
+    useSlide()
+})
+
 defineOptions({
     inheritAttrs: false
 })
 defineProps({
-    dataCarousel: {
+    dataSlideMotion: {
         type: String,
-        default: 'static'
+        default: 'slide'
     }
-
 })
 </script>
+
 <template>
-    <div class="relative w-full" :data-carousel="dataCarousel">
-        <!-- Carousel wrapper -->
-        <!--h-56 md:h-96-->
-        <div class="relative h-[26rem] overflow-hidden " v-bind="$attrs">
-            <!--Items-->
+    <div class="relative" :data-slide-motion="dataSlideMotion">
+        <div class="slides-container grid grid-flow-col gap-0 overflow-auto overflow-y-hidden snap-x snap-mandatory scroll-smooth touch-manipulation"
+            style="grid-auto-columns: 100% !important;" data-slide-container v-bind="$attrs">
             <slot></slot>
+            <!-- Slider controls -->
+            <slot name="carousel-controls">
+                <CarouselControls></CarouselControls>
+            </slot>
         </div>
-        <!-- Carousel controls -->
-        <slot name="carousel-controls">
-            <CarouselControls></CarouselControls>
-        </slot>
     </div>
 </template>
+
+<style scoped>
+:slotted(div) {
+    @apply snap-start duration-1000 ease-in-out;
+}
+</style> 
+
+
