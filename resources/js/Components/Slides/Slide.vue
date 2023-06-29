dc0d45f89e7e526eaa460009227f7d8163f3aa71
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-29 22:13:05 +0100

    overhauled the whole code so height wont have to be specified

--- resources/js/Components/Slides/Slide.vue
+++ resources/js/Components/Slides/Slide.vue
@@ -1,41 +1,34 @@
 <script setup>
 import { onMounted } from "vue";
 import SlideControls from "./SlideControls.vue";
+import { useSlide } from "../../Composables/Slide";
+
 onMounted(() => {
-    //Get all slideContainers
-    const slideContainers = document.querySelectorAll("[data-slide-container]");
+    useSlide()
+})
 
-    for (var i = 0; i < slideContainers.length; i++) {
-        //find the respective controls in each container
-        let slideContainer = slideContainers[i];
-        let nextButton =
-            slideContainers[i].querySelectorAll("[data-slide-next]")[0];
-        let previousButton = slideContainers[i].querySelectorAll(
-            "[data-slide-previous]"
-        )[0];
+defineOptions({
+    inheritAttrs: false
+})
 
-        const slideDistance = window.screen.availWidth / 2.5;
-        //add a click event to each of the slide
-        nextButton.addEventListener("click", () => {
-            slideContainer.scrollLeft += slideDistance;
-        });
-
-        previousButton.addEventListener("click", () => {
-            slideContainer.scrollLeft -= slideDistance;
-        });
+defineProps({
+    dataSlideMotion: {
+        type: String,
+        default: 'static'
     }
-});
+})
 </script>
 
 <!-- https://codepen.io/disguy-droid/details/wvxrPEL -->
 <template>
-    <div class="relative">
+    <div class="relative" :data-slide-motion="dataSlideMotion">
         <!--slides-container h-72 flex snap-x snap-mandatory overflow-hidden overflow-x-auto space-x-2 rounded scroll-smooth before:w-[45vw] before:shrink-0 after:w-[45vw] after:shrink-0 md:before:w-0 md:after:w-0
         slides-container flex snap-x snap-mandatory overflow-hidden space-x-2 scroll-smooth overflow-x-hidden
         
+        slides-container flex snap-x snap-mandatory space-x-2 scroll-smooth overflow-x-auto
         -->
-        <div class="slides-container flex snap-x snap-mandatory space-x-2 scroll-smooth overflow-x-auto"
-            data-slide-container>
+        <div class="slides-container grid grid-flow-col auto-cols-max gap-4 overflow-auto overflow-y-hidden snap-x snap-mandatory scroll-smooth touch-manipulation"
+            data-slide-container v-bind="$attrs">
             <slot></slot>
             <!-- Slider controls -->
             <slot name="slide-controls">
@@ -44,22 +37,8 @@ onMounted(() => {
         </div>
     </div>
 </template>
-
-<style>
-.slides-container {
-    -ms-overflow-style: none;
-    /* Internet Explorer 10+ */
-    scrollbar-width: none;
-    /* Firefox */
-}
-
-.slides-container::-webkit-scrollbar {
-    display: none;
-    /* Safari and Chrome */
-}
-</style>
 <style scoped>
-/*:slotted(div) {
-    @apply w-auto;
-}*/
+:slotted(div) {
+    @apply snap-always snap-center;
+}
 </style> 
