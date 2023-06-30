a3a2e10a1529144d760dce6be43e9a21eb9ddc3f
Kekeocha Justin <justinratzinger@gmail.com>
2023-06-30 21:00:36 +0100

    refactored slide.js

--- resources/js/Composables/Slide.js
+++ resources/js/Composables/Slide.js
@@ -1,35 +1,27 @@
 export function useSlide() {
   //Get all slideContainers
   const slideContainers = document.querySelectorAll("[data-slide-container]");
-
-  for (var i = 0; i < slideContainers.length; i++) {
-    //find the respective controls in each container
-    let slideContainer = slideContainers[i];
-    let slideContainerMotion =
-      slideContainer.parentElement.attributes["data-slide-motion"].value;
-
-    const slideDistance = window.screen.availWidth / 1.5;
-
+  const slideDistance = window.screen.availWidth / 1.5;
+  // Iterate over each container
+  slideContainers.forEach((container) => {
+    let containerMotion =
+      container.parentElement.attributes["data-slide-motion"].value;
     //Implement automatic slide
-    if (slideContainerMotion == "slide") {
+    if (containerMotion == "slide") {
       setInterval(function () {
-        slideContainer.scrollLeft += slideDistance;
+        container.scrollLeft += slideDistance;
       }, 5000);
     }
-
-    let nextButton =
-      slideContainers[i].querySelectorAll("[data-slide-next]")[0];
-    let previousButton = slideContainers[i].querySelectorAll(
-      "[data-slide-previous]"
-    )[0];
+    let nextButton = container.querySelectorAll("[data-slide-next]")[0];
+    let previousButton = container.querySelectorAll("[data-slide-previous]")[0];
 
     //add a click event to each of the slide
     nextButton.addEventListener("click", () => {
-      slideContainer.scrollLeft += slideDistance;
+      container.scrollLeft += slideDistance;
     });
 
     previousButton.addEventListener("click", () => {
-      slideContainer.scrollLeft -= slideDistance;
+      container.scrollLeft -= slideDistance;
     });
-  }
+  });
 }
