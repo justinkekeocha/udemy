ddc115ddcb69d91a6b1e689ba70c1a51884b971d
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-01 23:42:35 +0100

    Factory for Topic resource

---
+++ database/factories/TopicFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Topic>
+ */
+class TopicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->unique()->word();
+        return [
+            'title' => ucfirst($title),
+            'description' =>  fake()->text(),
+            'slug' => Str::slug($title, '-'),
+        ];
+    }
+}
