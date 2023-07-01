7255f9aa75c16d39686237b8d483145317b9dbc9
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-01 23:41:27 +0100

    added and redefined relationships

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class SubCategory extends Model
 {
@@ -16,19 +19,24 @@ class SubCategory extends Model
     }
 
     //Relationships
-
-    public function courses()
+    public function category(): BelongsTo
     {
-        return $this->hasMany(Course::class);
+        return $this->belongsTo(Category::class);
+    }
+    public function topics(): HasMany
+    {
+        $this->hasMany(Topic::class);
+    }
+    public function courses(): HasManyThrough
+    {
+        return $this->hasManyThrough(Course::class, Topic::class);
     }
 
-
     //Attributes
-
     protected function link(): Attribute
     {
         return Attribute::make(
-            get: fn () => route('subCategory.show', ['subCategory' => $this->slug])
+            get: fn () => route('subCategories.show', ['subCategory' => $this->slug])
         );
     }
 }
