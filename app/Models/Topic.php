e27ccefedcff9ddc66b9d7e89c561dcd0d841fdb
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-01 23:42:21 +0100

    Model for Topic resource

---
+++ app/Models/Topic.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    //Relationships
+    public function category(): HasOneThrough
+    {
+        return $this->hasOneThrough(Category::class, SubCategory::class, 'id', 'id', 'sub_category_id', 'category_id');
+    }
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    //Attributes
+    protected function link(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => route('topics.show', ['topic' => $this->slug])
+        );
+    }
+}
