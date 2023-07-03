ae4f361b2140b990aa2299673900356ce639e5f0
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-03 21:12:44 +0100

    changed URI names of route endpoint to match naming convention

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Models\Topic;
+use App\Models\Course;
 use App\Models\Category;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use App\Http\Resources\TopicResource;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\CourseResource;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\SubCategoryResource;
 
@@ -22,10 +26,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/category', function () {
-    return CategoryResource::collection(Category::all());
+Route::get('/categories', function () {
+    return CategoryResource::collection(Category::with(['subCategories', 'topics'])->get());
 })->name('categories.index');
 
-Route::get('/sub-category', function () {
+Route::get('/sub-categories', function () {
     return SubCategoryResource::collection(SubCategory::all());
 })->name('subCategories.index');
+
+
+Route::get('/topics', function () {
+    return TopicResource::collection(Topic::all());
+})->name('topics.index');
+
+Route::get('/courses', function () {
+    return CourseResource::collection(Course::all());
+})->name('courses.index');
