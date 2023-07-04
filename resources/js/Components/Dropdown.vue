41dc4c5b2c015aea335d83dcc22a8d04356d123f
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-04 23:30:36 +0100

    changed w-44 to w-full

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="z-50  hidden bg-white divide-y divide-gray-100 shadow w-44 dark:bg-gray-700">
+    <div class="z-50  hidden bg-white divide-y divide-gray-100 shadow w-full dark:bg-gray-700">
         <slot></slot>
     </div>
 </template>
