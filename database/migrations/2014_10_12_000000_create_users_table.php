a15ed96a33826992efbd54c2d875f2054988f3e5
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-05 21:36:07 +0100

    Added slug and image columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+
+            $table->string('slug')->unique();
+            $table->string('image');
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
