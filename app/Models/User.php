1c236bf54c85022a4d328cadced1e3fbd695fd53
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-05 21:35:17 +0100

    Changed the routeKeyName()

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -43,9 +45,22 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     //Relationships
-    public function instructedCourses()
+    public function instructedCourses(): HasMany
     {
         return  $this->hasMany(Course::class, 'instructor_id');
+    }
+
+    //Attributes
+    protected function link(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => route('users.show', ['user' => $this->slug])
+        );
     }
 }
