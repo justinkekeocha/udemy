9eb5b892cf40b2d34f3ab4baf992468174a7c943
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-06 21:15:50 +0100

    changed slot tag

--- resources/js/Components/Buttons/Button1.vue
+++ resources/js/Components/Buttons/Button1.vue
@@ -1,6 +1,6 @@
 <template>
    <button type="button"
       class="border border-black mb-5 text-gray-900 font-bold focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 text-sm px-5 py-2.5 mr-2 ">
-      <slot />
+     <slot></slot>
    </button>
 </template>
