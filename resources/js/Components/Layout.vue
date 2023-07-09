ca4fe4ffcdca36aca7eacdde8076a4027f1fec29
Kekeocha Justin <justinratzinger@gmail.com>
2023-07-09 20:54:51 +0100

    Changed stores to be called from navbar

--- resources/js/Components/Layout.vue
+++ resources/js/Components/Layout.vue
@@ -3,9 +3,21 @@ import Navbar from './Navbar.vue'
 import Footer from './Footer.vue';
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
+import { useCategoryStore } from "@/Stores/CategoryStore";
+import { useSubCategoryStore } from "@/Stores/SubCategoryStore";
+import { useTopicStore } from "@/Stores/TopicStore";
 defineProps(['title', 'description'])
 //https://flowbite.com/docs/getting-started/vue/#data-attributes
 // initialize components based on data attribute selectors
+const categoryStore = useCategoryStore();
+categoryStore.getCategories();
+
+const subCategoryStore = useSubCategoryStore();
+subCategoryStore.getSubCategories();
+
+const topicStore = useTopicStore();
+topicStore.getTopics()
+
 onMounted(() => {
     setTimeout(async () => {
         await initFlowbite();
@@ -13,6 +25,7 @@ onMounted(() => {
 
 })
 </script>
+
 
 <template>
     <Head>
