cc018b0d3ccdeea2965168003bf99d52bb21b0eb
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-08 22:29:44 +0100

    Add description in field

--- database/migrations/2023_06_01_160452_create_courses_table.php
+++ database/migrations/2023_06_01_160452_create_courses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('title');
-            $table->string('description');
+            $table->string('description', 2000);
             $table->integer('price');
             $table->string('image');
             $table->string('slug')->unique();
