c914d34bcbb7a095ae6d321699f27bf776bc549a
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-08 22:26:40 +0100

    Create rating resoruce

---
+++ app/Http/Resources/RatingResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RatingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'rating' => $this->rating,
+            'review' => $this->review,
+            'user' => new UserResource($this->user),
+        ];
+    }
+}
