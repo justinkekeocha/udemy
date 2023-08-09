8ab5f4010fe6b2324d6dde166583b99e264aee27
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-09 21:08:17 +0100

    Add instructed_courses column

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\CourseResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
@@ -21,6 +22,7 @@ class UserResource extends JsonResource
             'image' => $this->image,
             'about' => $this->about,
             'instructed_courses_count' => $this->instructed_courses_count,
+            'instructed_courses' => CourseResource::collection($this->whenLoaded('instructedCourses')),
         ];
     }
 }
