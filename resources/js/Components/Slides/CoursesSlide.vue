913f37bb03515dad47d269df071482307e0369bd
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-09 21:10:30 +0100

    Move code to the CourseCard component

--- resources/js/Components/Slides/CoursesSlide.vue
+++ resources/js/Components/Slides/CoursesSlide.vue
@@ -1,32 +1,12 @@
 <script setup>
 import Slide from './Slide.vue';
-import StarRating from '../StarRating.vue'
+import CourseCard from '../Cards/CourseCard.vue';
 defineProps({ courses: Object })
 </script>
 <template>
     <Slide>
         <template v-for="(row, index) in courses" :key="row.id">
-            <div class="flex flex-col max-w-[15rem]">
-                <Link class="my-5" :href="row.link">
-                <img class="w-full h-32 object-cover border border-stone-400 hover:transition-opacity hover:opacity-80"
-                    :src="row.image" alt="" />
-                <div class="p-5">
-                    <h3 class="text-base line-clamp-2 text-ellipsis tracking-tight mb-0">
-                        {{ row.title }}
-                    </h3>
-                    <p class="text-xs line-clamp-1 text-ellipsis text-gray-400">{{
-                        row.instructor.name }}</p>
-                    <div class="flex items-center">
-                        <StarRating :rating="row.rating" />
-                        <span class="text-gray-400 text-xs">({{ row.ratings_count }}) </span>
-                    </div>
-                    <p class="mb-3 font-UdemySansBold font-black">₦{{ row.price }} <span
-                            class="line-through text-gray-400 text-sm font-normal ms-1">₦{{
-                                row.inflated_price }}</span>
-                    </p>
-                </div>
-                </Link>
-            </div>
+            <CourseCard :course="row"></CourseCard>
         </template>
     </Slide>
 </template>
