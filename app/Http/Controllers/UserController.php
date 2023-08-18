c0a16c53312fd58a31432926cf14a6ebcc20ec57
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-18 22:24:16 +0100

    Feat: logout functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -60,5 +61,17 @@ class UserController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function logout(Request $request)
+    {
+
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
