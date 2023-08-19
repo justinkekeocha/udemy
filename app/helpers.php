ccdaa6ce8785f1692f97dd6f7660ecc30c955857
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-19 11:58:18 +0100

    Feat: create helper file

---
+++ app/helpers.php
@@ -0,0 +1,10 @@
+<?
+function moveMarchDate($date)
+{
+    $time = strtotime($date);
+    $month = date("F", $time);
+    $hour = date("Y", $time);
+
+    if ($month == 'March' && $hour == 2) {
+    }
+}
