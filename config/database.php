c5f1dedf578ce8df6769478b78dd9599c658ccde
Kekeocha Justin <justinratzinger@gmail.com>
2023-08-19 11:59:00 +0100

    Fix: fix error during migration on production

--- config/database.php
+++ config/database.php
@@ -57,10 +57,12 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            /**Below line helps to reset some shared hosting MYSQL settings**/
+            'engine' => 'InnoDB',
+            'timezone'  => '+00:00'
         ],
 
         'pgsql' => [
